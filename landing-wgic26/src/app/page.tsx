795d71a34594466341198f7c5eaa1f40d4424220
Jordi Poblet <hello@jordipoblet.com>
2025-05-17 00:55:17 +0200

    navigation

--- landing-wgic26/src/app/page.tsx
+++ landing-wgic26/src/app/page.tsx
@@ -1,22 +1,33 @@
-import HeaderComponent from "@/components/HeaderComponent";
+import { NavigationComponent } from "@/components/NavigationComponent";
 
 export default function Home() {
   return (
-    <div>
-      <HeaderComponent />
-      <main className="flex flex-col items-center">
-        <h1>World <strong>Green</strong> Infrastructure Congress 2026 <strong>Barcelona</strong></h1>
-
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <h4>Heading 4</h4>
-        <h5>Heading 5</h5>
-        <h6>Body Large</h6>
-        <p>Body Small</p>
+    <div className="flex flex-col gap-0">
+      <NavigationComponent />
+      <main className="mt-32">
+        <section className="flex flex-col items-center pt-10 bg-[darkgreen] p-10">
+          Section 1
+          <h1>World <strong>Green</strong> Infrastructure Congress 2026 <strong>Barcelona</strong></h1>
+        </section>
+        <section className="flex items-center justify-center bg-[purple] p-10">
+          Section 2
+        </section>
+        <section className="flex items-center justify-center bg-[darkblue] p-10">
+          Section 3
+        </section>
+        <section className="flex flex-col items-center justify-center bg-[darkred] p-10">
+          <h1>Heading 1</h1>
+          <h2>Heading 2</h2>
+          <h3>Heading 3</h3>
+          <h4>Heading 4</h4>
+          <h5>Heading 5</h5>
+          <h6>Body Large</h6>
+          <p>Body Small</p>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        FOOTER
+      <footer className="bg-[gray] flex justify-between px-24 py-20 w-full font-medium">
+        <h4 className="font-medium w-1/8">World <strong>Green</strong> Infrastructure Congress 2026 <strong>Barcelona</strong></h4>
+        <p>Footer Right</p>
       </footer>
     </div>
   );
--- landing-wgic26/src/components/NavigationComponent.tsx
+++ landing-wgic26/src/components/NavigationComponent.tsx
@@ -1,59 +1,283 @@
-import * as React from "react"
+import { ArrowRight, Book, Menu, Sunset, Trees, Zap } from "lucide-react";
 
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
-import Link from "next/link"
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
-
-export default function NavigationComponent() {
-    return (
-        <div>
-            <NavigationMenu>
-                <NavigationMenuList>
-                    <NavigationMenuItem>
-                        <Link href="/docs" passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                Section One
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger>Section Two</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <NavigationMenuLink>Page 1</NavigationMenuLink>
-                            <NavigationMenuLink>Page 2</NavigationMenuLink>
-                            <NavigationMenuLink>Page 3</NavigationMenuLink>
-                            <NavigationMenuLink>Page 4</NavigationMenuLink>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <Link href="/docs" passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                Section Three
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger>Section Four</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <NavigationMenuLink>Page 1</NavigationMenuLink>
-                            <NavigationMenuLink>Page 2</NavigationMenuLink>
-                            <NavigationMenuLink>Page 3</NavigationMenuLink>
-                            <NavigationMenuLink>Page 4</NavigationMenuLink>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
-
-                </NavigationMenuList>
-            </NavigationMenu>
-        </div>
-    );
+interface MenuItem {
+  title: string;
+  url: string;
+  description?: string;
+  icon?: React.ReactNode;
+  items?: MenuItem[];
 }
+
+interface NavigationComponentProps {
+  logo?: {
+    url: string;
+    src: string;
+    alt: string;
+    title: string;
+  };
+  menu?: MenuItem[];
+  actions?: {
+    button1: {
+      title: string;
+      url: string;
+    };
+  };
+}
+
+const NavigationComponent = ({
+  logo = {
+    url: "https://www.shadcnblocks.com",
+    src: "https://shadcnblocks.com/images/block/logos/shadcnblockscom-icon.svg",
+    alt: "logo",
+    title: "WGIC26",
+  },
+  menu = [
+    { title: "Program 2026", url: "#" },
+    {
+      title: "About",
+      url: "#",
+      items: [
+        {
+          title: "Blog",
+          description: "The latest industry news, updates, and info",
+          icon: <ArrowRight className="size-4 shrink-0" />,
+          url: "#",
+        },
+        {
+          title: "Company",
+          description: "Our mission is to innovate and empower the world",
+          icon: <ArrowRight className="size-4 shrink-0" />,
+          url: "#",
+        },
+        {
+          title: "Careers",
+          description: "Browse job listing and discover our workspace",
+          icon: <ArrowRight className="size-4 shrink-0" />,
+          url: "#",
+        },
+        {
+          title: "Support",
+          description:
+            "Get in touch with our support team or visit our community forums",
+          icon: <ArrowRight className="size-4 shrink-0" />,
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Location & Stay",
+      url: "#",
+      items: [
+        {
+          title: "Help Center",
+          description: "Get all the answers you need right here",
+          icon: <ArrowRight className="size-4 shrink-0" />,
+          url: "#",
+        },
+        {
+          title: "Contact Us",
+          description: "We are here to help you with any questions you have",
+          icon: <ArrowRight className="size-4 shrink-0" />,
+          url: "#",
+        },
+        {
+          title: "Status",
+          description: "Check the current status of our services and APIs",
+          icon: <ArrowRight className="size-4 shrink-0" />,
+          url: "#",
+        },
+        {
+          title: "Terms of Service",
+          description: "Our terms and conditions for using our services",
+          icon: <ArrowRight className="size-4 shrink-0" />,
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Speakers",
+      url: "#",
+    },
+    {
+      title: "Submissions",
+      url: "#",
+      items: [
+        {
+          title: "Help Center",
+          description: "Get all the answers you need right here",
+          icon: <ArrowRight className="size-4 shrink-0" />,
+          url: "#",
+        },
+        {
+          title: "Contact Us",
+          description: "We are here to help you with any questions you have",
+          icon: <ArrowRight className="size-4 shrink-0" />,
+          url: "#",
+        }
+      ],
+    },
+  ],
+  actions = {
+    button1: { title: "Button 1", url: "#" },
+  },
+}: NavigationComponentProps) => {
+  return (
+    <section className="py-10 px-12 justify-between w-full fixed bg-background">
+      {/* Desktop Menu */}
+      <nav className="hidden justify-between lg:flex w-full">
+        <div className="flex justify-start items-center gap-6 w-full">
+          {/* Logo */}
+          <a href={logo.url} className="flex items-center gap-2">
+            {/* <img src={logo.src} className="max-h-8" alt={logo.alt} /> */}
+            <span className="text-lg">
+              {logo.title}
+            </span>
+          </a>
+        </div>
+        <NavigationMenu>
+          <NavigationMenuList className="flex 2xl:gap-8 xl:gap-2">
+            {menu.map((item) => renderMenuItem(item))}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <div className="flex gap-2 w-full justify-end">
+          <Button asChild>
+            <a href={actions.button1.url}>{actions.button1.title}</a>
+          </Button>
+        </div>
+      </nav>
+
+      {/* Mobile Menu */}
+      <div className="block lg:hidden">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <a href={logo.url} className="flex items-center gap-2">
+            <img src={logo.src} className="max-h-8" alt={logo.alt} />
+          </a>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Menu className="size-4" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="overflow-y-auto">
+              <SheetHeader>
+                <SheetTitle>
+                  <a href={logo.url} className="flex items-center gap-2">
+                    <img src={logo.src} className="max-h-8" alt={logo.alt} />
+                  </a>
+                </SheetTitle>
+              </SheetHeader>
+              <div className="flex flex-col gap-6 p-4">
+                <Accordion
+                  type="single"
+                  collapsible
+                  className="flex w-full flex-col gap-4"
+                >
+                  {menu.map((item) => renderMobileMenuItem(item))}
+                </Accordion>
+
+                <div className="flex flex-col gap-3">
+                  <Button asChild>
+                    <a href={actions.button1.url}>{actions.button1.title}</a>
+                  </Button>
+                </div>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+const renderMenuItem = (item: MenuItem) => {
+  if (item.items) {
+    return (
+      <NavigationMenuItem key={item.title}>
+        <NavigationMenuTrigger
+        >{item.title}</NavigationMenuTrigger>
+        <NavigationMenuContent className="flex flex-col pt-4 pb-5 px-6">
+          {item.items.map((subItem) => (
+            <NavigationMenuLink asChild key={subItem.title} className="w-max hover:bg-transparent hover:text-white normal-case">
+              <SubMenuLink item={subItem} />
+            </NavigationMenuLink>
+          ))}
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    );
+  }
+
+  return (
+    <NavigationMenuItem key={item.title}>
+      <NavigationMenuLink
+        href={item.url}>
+        {item.title}
+      </NavigationMenuLink>
+    </NavigationMenuItem>
+  );
+};
+
+const renderMobileMenuItem = (item: MenuItem) => {
+  if (item.items) {
+    return (
+      <AccordionItem key={item.title} value={item.title} className="border-b-0">
+        <AccordionTrigger className="text-md py-0 font-semibold hover:no-underline">
+          {item.title}
+        </AccordionTrigger>
+        <AccordionContent className="mt-2">
+          {item.items.map((subItem) => (
+            <SubMenuLink key={subItem.title} item={subItem} />
+          ))}
+        </AccordionContent>
+      </AccordionItem>
+    );
+  }
+
+  return (
+    <a key={item.title} href={item.url} className="text-md font-semibold">
+      {item.title}
+    </a>
+  );
+};
+
+const SubMenuLink = ({ item }: { item: MenuItem }) => {
+  return (
+    <a
+      className="flex flex-row gap-4 justify-start rounded-none leading-none transition-none outline-none select-none normal-case hover:underline hover:underline-offset-8 hover:decoration-2 hover:decoration-accent"
+      href={item.url}
+    >
+      <div className="flex gap-2 py-2">
+        <div className="text-xs p-0">{item.title}</div>
+        {/* {item.description && (
+            <p className="text-xs">{item.description}</p>
+          )} */}
+        {/* <div className="text-accent">{item.icon}</div> */}
+      </div>
+    </a>
+  );
+};
+
+export { NavigationComponent };
