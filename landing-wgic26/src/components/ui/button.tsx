579c5b4cd944d8779ebf3740f8e5a8452a06f771
Jordi Poblet <hello@jordipoblet.com>
2025-05-19 14:21:04 +0200

    test commit

--- landing-wgic26/src/components/ui/button.tsx
+++ landing-wgic26/src/components/ui/button.tsx
@@ -21,13 +21,13 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        cube: "text-black bg-accent",
+        cube: "text-black bg-accent border-black",
       },
       size: {
         default: "h-10 px-4 py-5 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "size-9",
+        icon: "size-10",
       },
     },
     defaultVariants: {
