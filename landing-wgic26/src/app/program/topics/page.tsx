909f636e044bc6dc3dc2b3de9ac177302a0f7724
Jordi Poblet <hello@jordipoblet.com>
2025-05-22 02:00:18 +0200

    small copy change

--- landing-wgic26/src/app/program/topics/page.tsx
+++ landing-wgic26/src/app/program/topics/page.tsx
@@ -2,7 +2,7 @@ const Topics = () => {
     return (
         <div className="p-8">
             <h1>Topics</h1>
-            <p>This page will cover the main themes and subject matter of the congress.</p>
+            <p>This page will cover the main topics during the congress.</p>
         </div>
     );
 };
