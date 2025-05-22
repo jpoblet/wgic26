f68dabbb9b9d9c24b8380b76804f892ea9f6a94b
Jordi Poblet <hello@jordipoblet.com>
2025-05-22 10:50:18 +0200

    Topics page tweak

--- landing-wgic26/src/app/program/topics/page.tsx
+++ landing-wgic26/src/app/program/topics/page.tsx
@@ -2,7 +2,7 @@ const Topics = () => {
     return (
         <div className="p-8">
             <h1>Topics</h1>
-            <p>This page will cover the main topics during the congress.</p>
+            <p>This page will cover the main topics of the congress.</p>
         </div>
     );
 };
